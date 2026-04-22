b2d739b8e1c60d73967976d193bda5241b745539
mahmoudbarzegar <hedesh.mb@gmail.com>
2026-04-22 16:27:11 +0330

    The 1_nested_loops_way example was added.

---
+++ python_switch_example/1_nested_loops_way.py
@@ -0,0 +1,8 @@
+switches = ["A", "B", "C"]
+results = []
+switches_states = [0, 1]
+
+for a in switches_states:
+    for b in switches_states:
+        for c in switches_states:
+            print(dict(zip(switches, (a,b,c))))
