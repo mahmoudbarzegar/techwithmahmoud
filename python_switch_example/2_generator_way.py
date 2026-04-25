44cd83116ef787a93829efed4361cb54692b75d0
mahmoudbarzegar <hedesh.mb@gmail.com>
2026-04-25 10:49:55 +0330

    The 2_generator_way was added.

---
+++ python_switch_example/2_generator_way.py
@@ -0,0 +1,16 @@
+from typing import Generator
+
+
+switches = ["A", "B", "C"]
+
+def generate_states(n: int) -> Generator[list[int], None, None]:
+    if n == 0:
+        yield []
+    else:
+        for state in generate_states(n - 1):
+            yield state + [0]
+            yield state + [1]
+
+
+for state in generate_states(len(switches)):
+    print(dict(zip(switches,state)))
