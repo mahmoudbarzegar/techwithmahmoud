7d81edcae0e803ccda3a62f7b7553785fffe6190
mahmoudbarzegar <hedesh.mb@gmail.com>
2026-04-27 18:10:41 +0330

    The 3_itertools_product_way.py was added.

---
+++ python_switch_example/3_itertools_product_way.py
@@ -0,0 +1,7 @@
+from itertools import product
+
+switches = ["A", "B", "C"]
+switches_states = [0, 1]
+
+for state in product(switches_states, repeat=len(switches)):
+    print(dict(zip(switches,state)))
