25eef1d00d7458d2063c35e0dbb4b1352dd23796
mahmoudbarzegar <hedesh.mb@gmail.com>
2026-06-01 01:25:08 +0330

    The example_4.py was added.

---
+++ python_protocol_type/example_4.py
@@ -0,0 +1,30 @@
+from typing import Protocol
+
+class Storage(Protocol):
+    def save(self, data:str) -> None:
+        ...
+
+# ✅ Works with database
+class Database:
+    def save(self, data:str) -> None: 
+        print(f"Saved to database: {data}")
+
+# ✅ Works with file
+class FileStorage:
+    # def save(self, data:str): 
+    #     print(f"Saved to file: {data}")
+    def read_from_file_storage(self,data:str):
+        print(f"Read data from: {data}")
+
+# ✅ Works with cloud
+class CloudStorage:
+    def save(self, data:str): 
+        print(f"Saved to cloud: {data}")
+
+def save_data(storage:Storage):
+    storage.save("data")  # Works with ANYTHING that has save()
+
+# All work without inheritance or interfaces!
+save_data(Database())
+save_data(FileStorage())
+save_data(CloudStorage())
