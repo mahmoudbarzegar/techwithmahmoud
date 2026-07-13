07d04da0552aab2db13f5f0a93c9f06ab62e1afb
mahmoudbarzegar <hedesh.mb@gmail.com>
2026-07-13 23:05:45 +0330

    The naming was modified at the example_2.py of the protocol.

--- python_protocol_type/example_2.py
+++ python_protocol_type/example_2.py
@@ -1,24 +1,35 @@
 from abc import ABC, abstractmethod
 from typing import Protocol
 
+
 # ABC - Explicit inheritance
 class Animal(ABC):
     @abstractmethod
-    def speak(self) -> str : pass
+    def speak(self) -> str:
+        pass
 
-class Dog_1(Animal):  # Must inherit
-    def speak(self) -> str: return "Woof"
 
-#******************************#    
+class DogOne(Animal):  # Must inherit
+    def speak(self) -> str:
+        return "Woof"
+
+
+# ******************************#
+
 
 # Protocol - Structural matching
 class Speaker(Protocol):
     def speak(self) -> str: ...
 
+
 class Dog:  # No inheritance!
-    def speak(self) -> str: return "Woof"
+    def speak(self) -> str:
+        return "Woof"
+
 
 class Person:  # No inheritance!
-    def speak(self) -> str: return "Hello"
+    def speak(self) -> str:
+        return "Hello"
+
 
 # Both work with Protocol!
