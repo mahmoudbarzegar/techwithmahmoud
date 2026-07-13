3c9912c6146ebc0e57d7c0680eed06edaea26bb1
mahmoudbarzegar <hedesh.mb@gmail.com>
2026-07-13 23:08:17 +0330

    All files were reformatted with the ruff.

--- python_protocol_type/example_3.py
+++ python_protocol_type/example_3.py
@@ -1,27 +1,31 @@
 from typing import Any
 
+
 # Duck Typing - Simple & Flexible
-def save_data(storage:Any):
+def save_data(storage: Any):
     storage.save("data")  # Works with ANYTHING that has save()
+
 
 # ✅ Works with database
 class Database:
-    def save(self, data: Any): 
+    def save(self, data: Any):
         print(f"Saved to database: {data}")
+
 
 # ✅ Works with file
 class FileStorage:
-    def save(self, data: Any): 
+    def save(self, data: Any):
         print(f"Saved to file: {data}")
+
 
 # ✅ Works with cloud
 class CloudStorage:
-    def save(self, data: Any): 
+    def save(self, data: Any):
         print(f"Saved to cloud: {data}")
 
     def nothing_method(self):
         print("Nothing to show")
-        
+
 
 # All work without inheritance or interfaces!
 save_data(Database())
